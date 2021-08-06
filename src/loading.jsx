d1dd486d020ae982720691dd97560278b2ba0bd4
Simon Kaine <simonkaine@gmail.com>
2021-08-06 12:08:53 -0700

    Have ASC/DESC dropdown working + Loading state + loading spinner working.

---
+++ src/loading.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Loading extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <img src='../assets/download.png'></img>
+         );
+    }
+}
+ 
+export default Loading;
