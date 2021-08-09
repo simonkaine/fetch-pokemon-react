79a5cb70a3d9c10ebd8e161880c9c6e8484f51ce
Simon Kaine <simonkaine@gmail.com>
2021-08-09 16:54:20 -0700

    Loading spinner rendering within details page.

--- src/pokeDetail.js
+++ src/pokeDetail.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import Loader from 'react-loader-spinner';
 
 class PokeDetailsPage extends Component {
-    state = { data: {} }
+    state = { data: {}, loading: false}
 
     loadPokemonData = async () => {
+
         const { pokeId } = this.props.match.params;
         const url = `https://pokedex-alchemy.herokuapp.com/api/pokedex/${pokeId}`;
         const response = await fetch(url);
         const data = await response.json();
         this.setState({data})
+
+        setTimeout(() => {
+            this.setState({ pokedex: data.results, loading: true });
+        }, 1000)
     }
 
     componentDidMount() {
@@ -20,6 +26,7 @@ class PokeDetailsPage extends Component {
         console.log(data)
         return ( 
             <div>
+                {!this.state.loading && <Loader type="TailSpin" color="#00BFFF" height={80} width={80} />}
                 <h3>This is the details page where a Pokemon will render</h3>
                 <p>{data.pokemon}</p>
                 <img src={data.url_image} alt="pokemon"></img>
